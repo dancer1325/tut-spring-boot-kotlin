709e54b802c16acb8e4b934377f0adce5e25bace
dancer1325 <alfredotic0809@gmail.com>
2023-07-03 17:30:28 +0200

    doc(): add alternatives into main

--- src/main/kotlin/com/example/blog/BlogApplication.kt
+++ src/main/kotlin/com/example/blog/BlogApplication.kt
@@ -1,5 +1,7 @@
 package com.example.blog
 
+import org.springframework.boot.Banner
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
@@ -10,4 +12,12 @@ class BlogApplication
 
 fun main(args: Array<String>) {
 	runApplication<BlogApplication>(*args)
+
+	// Adding code
+//	runApplication<BlogApplication>(*args) {
+//		setBannerMode(Banner.Mode.OFF)		// Specify the SpringApplication.bannerMode
+//	}
+
+	// Alternative to run the SpringBoot application
+//	SpringApplication.run(BlogApplication::class.java, *args)
 }
